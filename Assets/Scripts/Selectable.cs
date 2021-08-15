92699b2aada66df8731ed25ddf5023e805de65b9
MikeTheAnomaly <methoff@gmail.com>
2021-08-14 20:09:40 -0400

    push

--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Selectable : MonoBehaviour
 {
+    public UnityEvent onHover = new UnityEvent();
+    public UnityEvent onSelected = new UnityEvent();
+    public UnityEvent deSelected = new UnityEvent();
+    public UnityEvent onDragUpdate = new UnityEvent();
+
     public virtual void OnHover()
     {
-
+        onHover.Invoke();
     }
 
     public virtual void OnSelected()
     {
-
+        onSelected.Invoke();
     }
 
-    public virtual void OnSelectedUpdate()
+    public virtual void DeSelected()
     {
+        deSelected.Invoke();
+    }
 
+
+    public virtual void OnDragUpdate()
+    {
+        onDragUpdate.Invoke();
     }
 
 }
